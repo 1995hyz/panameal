bd7b391106438d86661a3da82454a084e7e8e304
1995hyz <1995hyz@gmail.com>
2019-03-20 00:30:47 -0400

    Test Sample Json POST

--- src/main/java/Controller/LoginController.java
+++ src/main/java/Controller/LoginController.java
@@ -1,7 +1,12 @@
 package Controller;
 
+import model.LoginForm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import service.LoginService;
 
@@ -9,9 +14,28 @@ import model.User;
 import java.util.HashMap;
 
 @RestController
-@RequestMapping("/login")
+
 public class LoginController {
 
     @Autowired
     LoginService ls;
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ResponseEntity getLoginForm(@RequestBody LoginForm loginForm) {
+        System.out.println(loginForm.getEmail());
+        System.out.println(loginForm.getPasswordHash());
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @RequestMapping("/")
+    public ResponseEntity<String> test() {
+        return new ResponseEntity<> ("Hello World!", HttpStatus.OK);
+    }
+
+    /* To test, use the following command:
+        curl localhost:8080/login
+        -i -H "Accept: application/json" -H "Content-Type:application/json"
+        -X POST --data '{"email":"test@cooper.edu", "passwordHash":"paaskldfjasd"}'
+     */
+
 }
--- src/main/java/service/LoginService.java
+++ src/main/java/service/LoginService.java
@@ -4,11 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class LoginService {
-    String email;
-    String passwordHash;
-    public LoginService(String email, String passwordHash) {
-        this.email = email;
-        this.passwordHash = passwordHash;
+    public LoginService() {
     }
 
 }
