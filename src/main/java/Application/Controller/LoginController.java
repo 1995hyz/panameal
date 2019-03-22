f1ab9815e3b3c7f345f0655acb838b6f9c8905dd
1995hyz <1995hyz@gmail.com>
2019-03-21 23:25:18 -0400

    Add Login Service

--- src/main/java/Application/Controller/LoginController.java
+++ src/main/java/Application/Controller/LoginController.java
@@ -2,6 +2,7 @@ package Application.Controller;
 
 import Application.model.LoginForm;
 import Application.model.UserDB;
+import Application.model.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,8 @@ import Application.service.LoginService;
 
 import Application.model.User;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 
@@ -20,9 +23,11 @@ public class LoginController {
 
     @Autowired
     LoginService ls;
+    @Autowired
+    private UserRepository userRepository;
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public ResponseEntity <LoginForm> getLoginForm(@RequestBody LoginForm loginForm) {
+    public ResponseEntity<Optional> getLoginForm(@RequestBody LoginForm loginForm) {
         /*System.out.println(loginForm.getEmail());
         System.out.println(loginForm.getPasswordHash());
         UserDB sampleUserDB = new UserDB();
@@ -39,7 +44,14 @@ public class LoginController {
             return new ResponseEntity <LoginForm> (loginForm, HttpStatus.UNAUTHORIZED);
         }*/
         //return ResponseEntity.ok(HttpStatus.OK);
-        return null;
+        //System.out.println(loginForm.getEmail());
+        Optional<User> curr_user = userRepository.findByEmail(loginForm.getEmail());
+        return new ResponseEntity <>(curr_user, HttpStatus.OK);
+        /*
+        To test, use the following command:
+            curl localhost:8080/login -i -H "Accept: application/json" -H "Content-Type:application/json"
+            -X POST --data "{"""email""":"""test@cooper.edu""", """username""":"""hello"""}"
+         */
     }
 
     @RequestMapping("/signup")
