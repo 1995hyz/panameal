c987875368c206dde4576c53480c37e422b1a45d
Yingzhi <1995hyz@gmail.com>
2019-04-30 19:52:00 -0400

    Fix Double

--- src/main/java/Application/model/FollowingRepository.java
+++ src/main/java/Application/model/FollowingRepository.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 public interface FollowingRepository extends CrudRepository<Following, Integer> {
-    Optional<Following> findByUseridAndAndFollowto(Integer userid, Integer followto);
+    Optional<Following> findByUseridAndFollowto(Integer userid, Integer followto);
     Iterable<Following> findAllByUserid(ArrayList<Integer> iterable);
     Iterable<Following> findAllByFollowto(ArrayList<Integer> iterable);
     Optional<Following> findByUserid(Integer userid);
