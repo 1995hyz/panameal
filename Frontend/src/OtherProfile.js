44b967aaf6080dfb0e49e6422d1228451d388b4b
kevinlin6543 <kevin.lin6543@gmail.com>
2019-04-30 16:49:04 -0400

    made progress on profile page

--- Frontend/src/OtherProfile.js
+++ Frontend/src/OtherProfile.js
@@ -12,6 +12,8 @@ import Grid from '@material-ui/core/Grid';
 import url from "./modules/url";
 import Typography from "./modules/components/Typography";
 import FeedTile from './modules/components/FeedTile';
+import Button from '@material-ui/core/Button';
+import AppAppBar from "./modules/views/AppAppBar";
 import {Card, CardActions, CardContent, CardHeader} from "@material-ui/core";
 import IconButton from "@material-ui/core/IconButton";
 import ShareIcon from '@material-ui/icons/Share';
@@ -40,7 +42,20 @@ const styles = theme => ({
         width: 200,
         height: 200,
     },
+    avatarText: {
+        fontSize: '120px',
+    },
+    desText: {
+        fontSize: '14px',
+        marginBottom: '-10px'
+    },
+    profText: {
+        fontSize: '28px',
+        marginBottom: '2vh',
+    },
 });
+
+var token = localStorage.getItem('authToken') ? localStorage.getItem('authToken') : "";
 
 class OtherProfile extends React.Component {
 
@@ -53,9 +68,13 @@ class OtherProfile extends React.Component {
                 'Content-Type': 'application/json',
             },
             body:
-                this.props.match.params.username,
+                JSON.stringify({
+                    email: this.state.cuseremail,
+                    username: this.state.username,
+                }),
         })
             .then(response => {
+                console.log(this.state);
                 if (response.status === 200) {
                     return response.json();
                 }
@@ -66,17 +85,22 @@ class OtherProfile extends React.Component {
                     posts: myJson.post,
                     fname: myJson.user.firstname,
                     lname: myJson.user.lastname,
+                    follow: !!myJson.user.followFlag,
                 });
             });
     };
     state = {
         sent: false,
         redirect: false,
+        useron: !!localStorage.getItem('authToken'),
         username: this.props.match.params.username,
         email: "",
         fname: "",
         lname: "",
         posts: [],
+        cuseremail: token,
+        follow: false,
+        followText: ["Follow", "Unfollow"],
     };
     componentWillMount() {
         this.getUser();
@@ -84,26 +108,56 @@ class OtherProfile extends React.Component {
     handleLike = index => {
         console.log(index);
     };
+    handleFollow = () => {
+        this.setState({follow: !this.state.follow});
+    };
+
 
     render() {
+        if (localStorage.getItem("authToken")) {
+            var bar = <FeedBar/>;
+        }
+        else {
+            var bar = <AppAppBar/>;
+        }
         const { classes } = this.props;
         return (
             <React.Fragment>
-                <FeedBar />
+                {bar}
                 <AppForm>
                     <Grid container spacing={24}>
                         <Grid item xs={12} sm={6} >
-                            <Avatar alt="Stock image" src={process.env.PUBLIC_URL+"dude.jpg"} className={classes.bigAvatar} />
+                            <Avatar aria-label={this.state.username} className={classes.bigAvatar}>
+                                <Typography variant={'h1'} className={classes.avatarText}>
+                                    {this.state.username.charAt(0)}
+                                </Typography>
+                            </Avatar>
                         </Grid>
                         <Grid item xs={12} sm={6}>
-                            <Typography >
+                            <Typography variant={'subtitle1'} className={classes.desText}>
+                                {'username:'}
+                            </Typography>
+                            <Typography variant={'h6'} className={classes.profText}>
                                 {this.state.username}
                             </Typography>
+                            <Typography variant={'h6'}>
+                                {this.state.fname + ' ' +this.state.lname}
+                            </Typography>
                         </Grid>
                         <Grid item xs={12} sm={6}>
                             <Typography >
-                                {this.state.fname + this.state.lname}
+                                {this.state.number}
                             </Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <Button
+                                variant="outlined"
+                                color="secondary"
+                                disabled={!this.state.useron}
+                                className={classes.button}
+                                onClick={this.handleFollow}>
+                                {this.state.followText[this.state.follow ? 1 : 0]}
+                            </Button>
                         </Grid>
 
                     </Grid>
@@ -111,35 +165,12 @@ class OtherProfile extends React.Component {
                         <FeedTile
                             className={classes}
                             username={this.state.username}
-                            content={post.content}
-                            postTime={post.post_time}
-                            postID={post.post_id}
+                            content={post.post.content}
+                            postTime={post.post.post_time}
+                            postID={post.post.post_id}
                             handleLikeButton={this.handleLike}
+                            key={{index}}
                         />
-                        /*<Card className={classes.card} key={index}>
-                            <CardHeader
-                                avatar={
-                                    <Avatar aria-label="Recipe" className={classes.avatar}>
-                                        {this.state.username.charAt(0)}
-                                    </Avatar>
-                                }
-                                title={this.state.username}
-                                subheader={post.post_time}
-                            />
-                            <CardContent>
-                                <Typography component="p">
-                                    {post.content}
-                                </Typography>
-                            </CardContent>
-                            <CardActions className={classes.actions} disableActionSpacing>
-                                <IconButton aria-label="Like">
-                                    <FavoriteIcon />
-                                </IconButton>
-                                <IconButton aria-label="Share">
-                                    <ShareIcon/>
-                                </IconButton>
-                            </CardActions>
-                        </Card>*/
                         )
                     )}
                 </AppForm>
